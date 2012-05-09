189895154e21792208567321adf3b2e26fedc1a6
Arnaud Rebts <arnaud.rebts@gmail.com>
2012-05-09 10:20:53 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torrentui::Application.config.secret_token = 'f491edb76ae704ff0b518c011151bd7166119ac1b242035c4ac3152c16f94296ab2c370747096025fa2f7682c5346fe241e83ab76382e53ba9063a7d7bf4ad6a'
