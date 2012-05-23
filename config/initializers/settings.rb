1a3f29ca0bf6c0c1ee377e565a01aa99dc925805
Arnaud Rebts <arnaud.rebts@gmail.com>
2012-05-24 00:52:53 +0200

    Changed default configuration for transmission download dir.

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,3 +1,3 @@
-AppSettings.defaults[:transmission_download_dir] = '/home/kid/transmission/downloads'
+AppSettings.defaults[:transmission_download_dir] = '/home/p2p/transmission/downloads'
 AppSettings.defaults[:transmission_rpc_host] = '127.0.0.1'
 AppSettings.defaults[:transmission_rpc_port] = 9091
