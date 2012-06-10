7c058a1528e7de515eb3d6adbd12468e76c74a38
Arnaud Rebts <arnaud.rebts@gmail.com>
2012-06-10 12:10:26 +0200

    Cleaned up transmission recipe.

--- config/recipes/transmission.rb
+++ config/recipes/transmission.rb
@@ -11,22 +11,10 @@ namespace :transmission do
 
   desc "Setup transmission configuration for this application"
   task :setup, roles: :app do
-    run "mkdir -p /home/#{user}/transmission/downloads"
-#     run "#{sudo} chown -R debian-transmission:debian-transmission /home/#{user}/transmission"
-#     # run "#{sudo} perl -i -p -e 's/^USER=debian-transmission/USER=#{user}/' /etc/init.d/transmission-daemon"
-#     
-#     stop
-    template "transmission_settings.json.erb", "/tmp/transmission_settings"
-    
-    unless remote_file_exists?("/etc/transmission-daemon/settings.json.copy")
-      run "#{sudo} mv /etc/transmission-daemon/settings.json /etc/transmission-daemon/settings.json.copy"
-    end
-    
-    run "mv /tmp/transmission_settings /home/#{user}/transmission/settings.json"
-    run "#{sudo} ln -nfs /home/#{user}/transmission/settings.json /etc/transmission-daemon/settings.json"
-    start
+    run "mkdir -p /home/#{user}/transmission"
+    template "transmission_settings.json.erb", "/home/#{user}/transmission/settings.json"
   end
-#   after "deploy:setup", "transmission:setup"
+  after "deploy:setup", "transmission:setup"
 
   %w[start stop restart reload].each do |command|
     desc "#{command} transmission"
