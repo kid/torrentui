8764ed5612f2f6622530ff7469bc194e8c6460e1
Arnaud Rebts <arnaud.rebts@gmail.com>
2013-02-18 20:30:26 +0100

    Adds multiple torrents upload using drag and drop on torrents page.

--- app/assets/javascripts/torrents.js
+++ app/assets/javascripts/torrents.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 	$("#torrents").tablesorter({});
+
+	var fileUploadErrors = {
+		maxFileSize: 'File is too big',
+		minFileSize: 'File is too small',
+		acceptFileTypes: 'Filetype not allowed',
+		maxNumberOfFiles: 'Max number of files exceeded',
+		uploadedBytes: 'Uploaded bytes exceed file size',
+		emptyResult: 'Empty file upload result'
+	};
+
+	// Initialize the jQuery File Upload widget:
+	$('#fileupload').fileupload({
+		acceptFileTypes: /\.torrent/i,
+
+		drop: function (e, data) {
+			// $('#new-torrent-dialog').modal();
+		},
+		done: function (e, data) {
+			console.log("done");
+		},
+		stop: function (e, data) {
+			window.location.reload();
+		}
+	});
 });
